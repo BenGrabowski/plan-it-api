95f9e5fb7112eb477f95d5915947f8768cc07591
Ben Grabowski <gbowski30@gmail.com>
2020-02-01 16:21:29 -0700

    added whitelist

--- src/app.js
+++ src/app.js
@@ -17,11 +17,22 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
+
+var whitelist = 'https://plan-it.now.sh';
+var corsOptions = {
+ origin: function (origin, callback) {
+   if (whitelist.indexOf(origin) !== -1) {
+     callback(null, true)
+   } else {
+     callback(new Error('Not allowed by CORS'))
+   }
+
+app.use(cors(corsOptions));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
