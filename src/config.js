175abf0e737d3f3ed1b38cb1f2e4c9740a6d28a3
Ben Grabowski <gbowski30@gmail.com>
2020-02-01 14:12:11 -0700

    fixes cors issue

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
+    CLIENT_ORIGIN: 'https://plan-it.now.sh',
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/planit',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
+    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
 };
