c66e334a5f41dd0c1dd07d79bf5fa71a5b3bf621
Ben Grabowski <gbowski30@gmail.com>
2020-02-01 17:42:25 -0700

    push feature branch to heroku

--- src/app.js
+++ src/app.js
@@ -17,25 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-// app.use(
-//     cors({
-//         origin: CLIENT_ORIGIN
-//     })
-// );
-
-const whitelist = ['https://plan-it.now.sh'];
-
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
